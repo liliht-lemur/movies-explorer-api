7fdef57527284b8922cf9ab714376020d4017c8e
Liliht <liliht@MacBook-Pro-Liliht.local>
2023-09-11 19:06:24 +0400

    turnoff cors

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const cors = require('cors');
 const { errors } = require('celebrate');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
@@ -19,8 +18,32 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { PORT = 3000, DB_URL = 'mongodb://localhost:27017/bitfilmsdb' } = process.env;
 
 const app = express();
+const allowedCors = [
+  'https://liliht.nomoredomains.sbs',
+  'http://liliht.nomoredomains.sbs',
+  'http://localhost:3000',
+  'localhost:3000',
+];
 
-app.use(cors());
+app.use((req, res, next) => {
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const { origin } = req.headers;
+  const { method } = req;
+  const requestHeaders = req.headers['access-control-request-headers'];
+
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Origin', '*');
+  }
+
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+
+  return next();
+});
 
 mongoose.connect(DB_URL);
 
